03da033eaabeec10d6c01725ec41885b872c57a1
K B <msegun@github.com>
2018-09-28 12:52:53 +0200

    Created customers and configured redis in settings managed to properly open and close WebSockets

---
+++ myLibrary/Authors/consumer.py
@@ -0,0 +1,10 @@
+from channels.generic.websocket import WebsocketConsumer
+
+
+class TimeConsumer(WebsocketConsumer):
+
+    def connect(self):
+        self.accept()
+
+    def disconnect(self, close_code):
+        pass
--- myLibrary/myLibrary/routing.py
+++ myLibrary/myLibrary/routing.py
@@ -1,6 +1,15 @@
 # myLibrary/o
-from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+import Authors.routing
+
+# Nie wiem dlaczego przy authors pokazuje bład ale działa
 
 application = ProtocolTypeRouter({
     # (http->django views is added by default)
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            Authors.routing.websocket_urlpatterns
+        )
+    ),
 })
